4a68250f047cccfcaf9e465e54d2e210df6f787e
Phi <phiphi.tan23@gmail.com>
2024-01-20 21:04:14 +0800

    Upload modules

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 import random
 import tkinter as tk
-from pathlib import Path
 import platform
 
-# Using pathlib.Path to have general paths regardless of OS
-RAW_IDLE_PATH = Path("assets/idle.gif")
-IDLE_PATH = RAW_IDLE_PATH.resolve()
-IDLE_PATH = "assets/idle.gif"
-RIGHT_PATH = "assets/ghost_right_move.gif"
-LEFT_PATH = "assets/ghost_left_move.gif"
+from ghost import load_images 
+from chatgpt import openChatGPTInput
+
 ANIMATION_DELAY = 150
 WINDOW_SIZE = 100
 OFFSET = 300
 
 window = tk.Tk()
+
+default, walk_right, walk_left = load_images()
 
 # position of virtual pet
 screen_width = window.winfo_screenwidth()
@@ -27,25 +25,7 @@ right_num = [5, 6, 7]
 left_num = [8, 9, 10]
 event_num = random.randrange(1, 3, 1)
 
-# gif inputs
-default = [
-    tk.PhotoImage(file=IDLE_PATH, format="gif -index %i" % (i)).zoom(2, 2)
-    for i in range(8)
-]
-
-walk_right = [
-    tk.PhotoImage(file=RIGHT_PATH, format="gif -index %i" % (i)).zoom(2, 2)
-    for i in range(8)
-]
-
-walk_left = [
-    tk.PhotoImage(file=LEFT_PATH, format="gif -index %i" % (i)).zoom(2, 2)
-    for i in range(8)  
-]
-
 # event number
-
-
 def event(cycle, current_state, event_num, pos, pet_widget):
     if event_num in default_num:
         current_state = 0
@@ -61,13 +41,12 @@ def event(cycle, current_state, event_num, pos, pet_widget):
 
 
 # gif movement
-
-
 def move(cycle, frame, event_num, first, last, pet_widget):
     if cycle < len(frame) - 1:
         cycle += 1
     else:
         cycle = 0
+        # FIX THIS FOR THE FREEZING ISSUE
         event_num = random.randrange(first, last + 1, 1)
     return cycle, event_num
 
@@ -93,14 +72,10 @@ def update(cycle, current_state, event_num, pos, pet_widget):
     canvas.itemconfig(pet_widget, image=frame)
     window.after(ANIMATION_DELAY, event, cycle, current_state, event_num, pos, pet_widget)
 
-def openChatGPTInput(event):
-    print('open')
-    textInput = tk.Toplevel(window)
-    textInput.geometry('200x200')
 
 
 canvas = tk.Canvas(window, width=100, height=100)
-canvas.bind('<Button-1>', openChatGPTInput)
+canvas.bind('<Button-1>', lambda event: openChatGPTInput(event, window))
 canvas.pack()
 canvas.focus_set()
 
@@ -123,6 +98,5 @@ elif platform.system() == "Windows":
 window.overrideredirect(True)
 window.geometry(f"{WINDOW_SIZE}x{WINDOW_SIZE}+" + str(pos) + f"+{OFFSET}")
 window.wm_attributes("-topmost", True)
-window.wm_attributes("-transparent", True)
 window.after(0, update, cycle, current_state, event_num, pos, pet_widget)
 window.mainloop()
