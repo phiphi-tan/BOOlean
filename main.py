c44772a7b8be3321ffb78800dcf52c9d826dd1bd
Martin <martin.ng@u.nus.edu>
2024-01-20 17:50:13 +0800

    display window

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import tkinter as tk
 
+IDLE_PATH = "assets/idle.gif"
+ANIMATION_DELAY = 150
+
 window = tk.Tk()
 
 # Position of virtual pet
@@ -9,43 +12,53 @@ pet_width = 48
 pos = (screen_width - pet_width) // 2
 
 cycle = 0
-current_state = 1
+current_state = 0
 default_num = [1, 2, 3, 4]
 event_num = random.randrange(1, 3, 1)
-impath = 'C:\\Users\\kkaiyu\\Downloads\\'
 
 # Default gif (8 frames)
-default = [tk.PhotoImage(file=impath+'default.gif', format = 'gif -index %i' %(i)) for i in range(8)]
+default = [
+    tk.PhotoImage(file=IDLE_PATH, format="gif -index %i" % (i)).zoom(2, 2)
+    for i in range(8)
+]
 
 # Assigning Number to event
+
+
 def event(cycle, current_state, event_num, pos):
     if event_num in default_num:
-        check = 0
-        print("default")
-        window.after(400, update, cycle, current_state, event_num, pos)
+        current_state = 0
+        window.after(0, update, cycle, current_state, event_num, pos)
+
 
 # Moving the GIF
+
+
 def gif_work(cycle, frame, event_num, first, last):
     if cycle < len(frame) - 1:
         cycle += 1
     else:
         cycle = 0
-        event_num = random.randrange(first, last + 1, 1)
-    return cycle, event
+        # event_num = random.randrange(first, last + 1, 1)
+    return cycle, event_num
 
-def update(cycle, current_state, event, pos):
-    if current_state == 0: # default
+
+def update(cycle, current_state, event_num, pos):
+    if current_state == 0:  # default
         frame = default[cycle]
         cycle, event_num = gif_work(cycle, default, event_num, 1, 9)
 
+    window.geometry("100x100+" + str(pos) + "+300")
+    label.configure(image=frame)
+    window.after(ANIMATION_DELAY, event, cycle, current_state, event_num, pos)
 
-window.config(highlightbackground='black')
-window.overrideredirect(True)
-window.wm_attributes('-transparentcolor', 'white')
 
-label = tk.Label(window, bd=0, bg='white')
+label = tk.Label(window, bd=0)
 label.pack()
-window.after(1, update, cycle, current_state, event_num, pos)
+
+window.config(bg="systemTransparent")
+window.overrideredirect(True)
+window.wm_attributes("-topmost", True)
+window.wm_attributes("-transparent", True)
+window.after(0, update, cycle, current_state, event_num, pos)
 window.mainloop()
-
-
